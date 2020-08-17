3e6325bf6d23787bcfc9b4d0c1b9f17d0ea916c6
alegoriyass <alegoriyass@yandex.ru>
2020-08-17 11:29:44 +0300

    Update and Edit Blog Post

--- ablog/theblog/views.py
+++ ablog/theblog/views.py
@@ -1,7 +1,7 @@
 from django.shortcuts import render
-from django.views.generic import ListView, DetailView, CreateView
+from django.views.generic import ListView, DetailView, CreateView, UpdateView
 from .models import Post
-from .forms import PostForm
+from .forms import PostForm, EditForm
 
 #def home(request):
     #return render(request, 'theblog/home.html', {})
@@ -20,3 +20,9 @@ class AddPostView(CreateView):
     template_name = 'theblog/add_post.html'
     #fields = '__all__'
     #fields = ('title', 'body')
+
+class UpdatePostView(UpdateView):
+    model = Post
+    form_class = EditForm
+    template_name = 'theblog/update_post.html'
+    #fields = ['title', 'title_tag', 'author', 'body']
