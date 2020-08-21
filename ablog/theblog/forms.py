1eb331ae05ea4ecf1ce5c3b082b6769da6b9e586
alegoriyass <alegoriyass@yandex.ru>
2020-08-21 15:36:50 +0300

    Add categories to our blog with Django

--- ablog/theblog/forms.py
+++ ablog/theblog/forms.py
@@ -1,15 +1,27 @@
 from django import forms
-from .models import Post
+from .models import Post, Category
+
+#from .models import Post
+
+#choices = [('coding', 'coding'), ('sports', 'sports'), ('entertainment', 'entertainment'),]
+choices = Category.objects.all().values_list('name', 'name')    # name from models.py class Category
+
+choice_list = []
+
+for item in choices:
+    choice_list.append(item)
 
 class PostForm(forms.ModelForm):
     class Meta:
         model = Post
-        fields = ('title', 'title_tag', 'author', 'body')
+        fields = ('title', 'title_tag', 'author', 'category', 'body')
 
         widgets = {
+            #'title': forms.TextInput(attrs={'class': 'form-control', 'placeholder': choices}),
             'title': forms.TextInput(attrs={'class': 'form-control'}),
             'title_tag': forms.TextInput(attrs={'class': 'form-control'}),
             'author': forms.Select(attrs={'class': 'form-control'}),
+            'category': forms.Select(choices=choice_list, attrs={'class': 'form-control'}),
             'body': forms.Textarea(attrs={'class': 'form-control'}),
             }
 
@@ -22,5 +34,6 @@ class EditForm(forms.ModelForm):
             'title': forms.TextInput(attrs={'class': 'form-control'}),
             'title_tag': forms.TextInput(attrs={'class': 'form-control'}),
             #'author': forms.Select(attrs={'class': 'form-control'}),
+            'category': forms.Select(choices=choices, attrs={'class': 'form-control'}),
             'body': forms.Textarea(attrs={'class': 'form-control'}),
             }
--- ablog/theblog/urls.py
+++ ablog/theblog/urls.py
@@ -4,7 +4,7 @@ Definition of urls for theblog.
 
 from django.urls import path
 from . import views
-from .views import HomeView, ArticleDetailView, AddPostView, UpdatePostView, DeletePostView
+from .views import HomeView, ArticleDetailView, AddPostView, UpdatePostView, DeletePostView, AddCategoryView
 
 urlpatterns = [
     path('', HomeView.as_view(), name='home'),
@@ -12,4 +12,6 @@ urlpatterns = [
     path('add_post/', AddPostView.as_view(), name='add_post'),
     path('article/edit/<int:pk>', UpdatePostView.as_view(), name='update_post'),
     path('article/<int:pk>/remove', DeletePostView.as_view(), name='delete_post'),
+
+    path('add_category/', AddCategoryView.as_view(), name='add_category'),
 ]
