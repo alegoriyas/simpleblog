e433249d2902aadaeecadd5ad5c48ee862a6ccee
alegoriyass <alegoriyass@yandex.ru>
2020-10-07 11:53:19 +0300

    restrict who can add blog posts

--- ablog/theblog/forms.py
+++ ablog/theblog/forms.py
@@ -5,11 +5,16 @@ from .models import Post, Category
 
 #choices = [('coding', 'coding'), ('sports', 'sports'), ('entertainment', 'entertainment'),]
 choices = Category.objects.all().values_list('name', 'name')    # name from models.py class Category
-
 choice_list = []
 
 for item in choices:
     choice_list.append(item)
+
+#choices_author = Post.objects.all().values_list('author', 'author')
+#choice_author_list = []
+
+#for item in choices_author:
+    #choice_author_list.append(item)
 
 class PostForm(forms.ModelForm):
     class Meta:
@@ -20,7 +25,8 @@ class PostForm(forms.ModelForm):
             #'title': forms.TextInput(attrs={'class': 'form-control', 'placeholder': choices}),
             'title': forms.TextInput(attrs={'class': 'form-control'}),
             'title_tag': forms.TextInput(attrs={'class': 'form-control'}),
-            'author': forms.Select(attrs={'class': 'form-control'}),
+            'author': forms.TextInput(attrs={'class': 'form-control', 'value': '', 'id':'elder', 'type':'hidden'}),
+            #'author': forms.Select(attrs={'class': 'form-control'}),
             'category': forms.Select(choices=choice_list, attrs={'class': 'form-control'}),
             'body': forms.Textarea(attrs={'class': 'form-control'}),
             }
